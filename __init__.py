34f4199cf830ab831ac7f50a39c39313b7f71941
andrew mendela <jurgeon018@gmail.com>
2020-09-03 13:34:53 +0000

    s

--- __init__.py
+++ __init__.py
@@ -1,28 +1,60 @@
 '''
-Інструкція для швидкої розгортки.
-cd projects
+
+
+
+
+
+
+
+
+cd ~/projects
 mkdir <project_name> 
 cd <project_name>
 python3 -m venv venv
 source venv/bin/activate
 
-******* 1 варіант
+
 mkdir src 
 cd src 
-git clone git@github.com:<username>/<project_name> . 
-*******
+git clone git@github.com:jurgeon/<project_name> . 
 
-******* 2 варіант
-git clone git@github.com:<username>/<project_name> src
-cd src 
-*******
 
 git clone git@github.com:jurgeon018/box
 cp -rp ./boilerplate/* . 
+
+
+cd sw_shop 
+git clone git@github.com:starwayagency/sw_shop .
+cd ..
+
+
+cd sw_blog 
+git clone git@github.com:starwayagency/sw_blog .
+cd ..
+
+
+cd sw_liqpay 
+git clone git@github.com:starwayagency/sw_liqpay .
+cd ..
+
+
 git add . 
-git commit -m 's'
+git commit -m 'initial commit'
 git push origin master 
+
+
 pip install -r ./box/core/requirements.txt
+
+
+
+
+
+
+
+
+
+
+
 '''
 
 
